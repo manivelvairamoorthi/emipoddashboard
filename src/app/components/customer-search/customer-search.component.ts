10e2b9be0ea40d0e34c2ea8c8334f1029bab2a17
Vikash Sinha <vikash.sinha@bajajfinserv.in>
2021-12-07 16:36:09 +0530

    done

--- src/app/components/customer-search/customer-search.component.ts
+++ src/app/components/customer-search/customer-search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {KpiService} from '../../services/kpi.service';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-customer-search',
@@ -10,11 +11,19 @@ export class CustomerSearchComponent implements OnInit {
   public kpiObject:any;
   public keys:any;
   public model: any = {};
+  today:Date= new Date();
+  fileName :string = "EmiPodCustomerSearchData.xlsx"
 
   constructor(private service: KpiService) { }
 
   ngOnInit(): void {
-
+    // this.model.mobileNumber = "8789246572";
+    // this.model.podRequestId = "12345";
+    this.today.setMinutes(this.today.getMinutes()+330);
+    this.model.endTime = this.today.toISOString().slice(0, 16);
+    this.today.setDate(this.today.getDate()-1);
+    this.model.startTime = this.today.toISOString().slice(0, 16);
+    console.log("date -"+this.model.startTime);
       
   }
   submit(){
@@ -23,6 +32,20 @@ export class CustomerSearchComponent implements OnInit {
         this.kpiObject=data.kpiItemList;
         this.keys = Object.keys(this.kpiObject[0]);
       });
+  }
+  exportexcel(): void
+  {
+    /* pass here the table id */
+    let element = document.getElementById('excel-table');
+    const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+ 
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+ 
+    /* save to file */  
+    XLSX.writeFile(wb, this.fileName);
+ 
   }
   
 }
