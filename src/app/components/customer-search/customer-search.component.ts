230f7351c91c4ffe5bc179ac0386d6c2e7d45217
Vikash Sinha <vikash.sinha@bajajfinserv.in>
2021-12-13 15:06:45 +0530

    no record found message added, unwanted code removed

--- src/app/components/customer-search/customer-search.component.ts
+++ src/app/components/customer-search/customer-search.component.ts
@@ -11,27 +11,24 @@ export class CustomerSearchComponent implements OnInit {
   public kpiObject:any;
   public keys:any;
   public model: any = {};
-  today:Date= new Date();
+  public emptyData : boolean = false;
   fileName :string = "EmiPodCustomerSearchData.xlsx"
 
   constructor(private service: KpiService) { }
 
   ngOnInit(): void {
-    // this.model.mobileNumber = "8789246572";
-    // this.model.podRequestId = "12345";
-    // this.today.setMinutes(this.today.getMinutes()+330);
-    // this.model.endTime = this.today.toISOString().slice(0, 16);
-    // this.today.setDate(this.today.getDate()-1);
-    // this.model.startTime = this.today.toISOString().slice(0, 16);
-    // console.log("date -"+this.model.startTime);
+    
       
   }
   submit(){
     this.service.fetchKpiData(this.model).
       subscribe((data: any)=>{
-        console.log(this.model);
-        console.log(data);
         this.kpiObject=data.kpiItemList;
+        if(this.kpiObject === null || Object.keys(this.kpiObject).length ==0){
+          this.emptyData = true;
+          return;
+        }
+        this.emptyData = false;
         this.keys = Object.keys(this.kpiObject[0]);
       });
   }
